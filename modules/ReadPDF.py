4b750603bb2ee87bfd9ea45d946edd8106e5f4f8
BecseiSzabolcs-dev <mr.vendeg748@gmail.com>
2026-04-02 13:08:34 +0200

    hotfix fix deleted "add macro.xlsm" fix corupted files

--- modules/ReadPDF.py
+++ modules/ReadPDF.py
@@ -4,12 +4,12 @@ try:
     from .GetData import GetData, Futam, Horses
 except:
     from GetData import GetData, Futam, Horses
-<<<<<<< HEAD
+
 
 def removeTXT(search, txt):
     if " "+search in txt: return txt[0:txt.index(" "+search)]
     elif search+" " in txt:   return txt[0:txt.index(search)]
-=======
+
 import re
 import sys
 from PyQt6.QtWidgets import (QApplication, QWidget, QVBoxLayout, 
@@ -41,7 +41,7 @@ class DatePickerPopup(QDialog):
 
 def removeTXT(search, txt):
     if search in txt:   return txt[0:txt.index(search)]
->>>>>>> dev
+
     else:                 return txt
 
 def remove_dupl(li:list):
@@ -52,38 +52,31 @@ def remove_dupl(li:list):
     
     return ndli
 
-<<<<<<< HEAD
-                       
-=======
+
 def clean_opinion(txt):
     # Find the last occurrence of ')' that belongs to a horse and cut after it
     match = re.search(r'(.*\(\d+\))', txt)
     if match:
         return match.group(1)
     return txt           
->>>>>>> dev
 
 
 class ReadPDF:
 
-<<<<<<< HEAD
-    def __init__(self, file_name):
-=======
+
     def __init__(self, mainWindow=None, file_name=""):
->>>>>>> dev
+
         self.rome_num = ["I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV"]
         self.horses = []
         self.futams = []
         self.opinions = []
         self.pdf = []
-<<<<<<< HEAD
-        self.read(file_name)
-=======
+
         self.date = []
         self.mainWindow = mainWindow
         self.read(file_name)
         
->>>>>>> dev
+
 
 
     def read(self,file_name):
@@ -100,10 +93,7 @@ class ReadPDF:
             text = page.extract_text()
             self.pdf.append(text.split("\n"))
 
-<<<<<<< HEAD
-        #print(file_name.split("/")[-1].split("_")[1:-1])
-        y,m,d = file_name.split("/")[-1].split("_")[1:-1]
-=======
+
         if("\\" in file_name): date = file_name.replace(".pdf","").split("\\")[-1].split("_")
         else:                  date = file_name.replace(".pdf","").split("/")[-1].split("_")
         to_remove = {"ugeto", "versenyprogram"}
@@ -121,7 +111,7 @@ class ReadPDF:
                 if len(date) == 3: y,m,d = date 
                 else: print("faild to load data")
 
->>>>>>> dev
+
 
         data = GetData(f"https://mla.kincsempark.hu/racecards/trotting/{y}-{m}-{d}")
         for ln in data.futam_data:
@@ -144,17 +134,8 @@ class ReadPDF:
                         break
 
         for i, op in enumerate(opinions):
-<<<<<<< HEAD
             for num in range(9, 14):
                 opinions[i] = removeTXT(str(num), opinions[i])
-=======
-            opinions[i] = opinions[i].strip()
-            opinions[i] = opinions[i].replace("\n",' ')
-            opinions[i] = opinions[i].replace("\t",' ')
-            
-            #for num in range(9, 14):
-                #opinions[i] = removeTXT(str(num), opinions[i])
->>>>>>> dev
 
             opinions[i] = removeTXT("Elérhetőségek", opinions[i])
             opinions[i] = removeTXT("100.000 Ft", opinions[i])
@@ -162,25 +143,10 @@ class ReadPDF:
             opinions[i] = removeTXT("300.000 Ft", opinions[i])
             opinions[i] = removeTXT("101.190 Ft", opinions[i])
             opinions[i] = removeTXT("Esélyelemzés", opinions[i])
-<<<<<<< HEAD
-=======
-            opinions[i] = removeTXT("Nyeregbe!", opinions[i])
-            opinions[i] = removeTXT("Az ügető", opinions[i])
-            opinions[i] = removeTXT("A futamban", opinions[i])
-            
-            opinions[i] = clean_opinion(opinions[i])
-
->>>>>>> dev
             #Véleményünk: 
             opinions[i] = opinions[i].replace("Véleményünk: ",'')
             opinions[i] = opinions[i].replace("Véleményünk:",'')
             opinions[i] = opinions[i].strip()
-<<<<<<< HEAD
-=======
-            
-            
-            
->>>>>>> dev
 
         
         self.opinions = remove_dupl(opinions)
@@ -203,19 +169,11 @@ class ReadPDF:
 
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     PDF_data = ReadPDF(r"C:\Users\Becsei Szabolcs\Downloads\versenyprogram_2025_12_13_ugeto.pdf")
 
     print("titles:")
     for i in PDF_data.futams:
         print(i)
-=======
-    PDF_data = ReadPDF(file_name=r"C:\Users\Becsei Szabolcs\Downloads\versenyprogram_2025_12_31_ugeto.pdf")
-
-    print("titles:")
-    for i in PDF_data.futams:
-        print(i.opinion)
->>>>>>> dev
 
 
 
